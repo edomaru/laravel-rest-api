5a932b58c9171a82462636f58dc6fa9ce0de56e1
Eding Muhamad <masaruedo@yahoo.com>
2025-05-12 23:04:40 +0700

    Add Register endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\Auth\RegisterController;
 use App\Http\Controllers\Api\V1\CompleteTaskController;
 use App\Http\Controllers\Api\V1\TaskController;
 use Illuminate\Http\Request;
@@ -17,4 +18,5 @@ Route::prefix('v1')->group(function () {
 
 Route::prefix('auth')->group(function () {
     Route::post('/login', LoginController::class);
+    Route::post('/register', RegisterController::class);
 });
--- tests/Feature/Api/Auth/AuthTest.php
+++ tests/Feature/Api/Auth/AuthTest.php
@@ -36,4 +36,37 @@ class AuthTest extends TestCase
         $response->assertStatus(422);
         $response->assertJsonValidationErrors(['email']);
     }
+
+    public function test_user_can_register_and_receive_token(): void
+    {
+        $payload = [
+            'name' => 'New user',
+            'email' => $email = 'newuser@example.com',
+            'password' => 'password123',
+            'password_confirmation' => 'password123'
+        ];
+
+        $response = $this->postJson('/api/auth/register', $payload);
+
+        $response->assertCreated();
+        $response->assertJsonStructure(['token', 'user']);
+        $this->assertDatabaseHas('users', [
+            'email' => $email
+        ]);
+    }
+    
+    public function test_user_cannot_register_with_invalid_data(): void
+    {
+        $payload = [
+            'name' => '',
+            'email' => $email = 'wrong-email',
+            'password' => 'short',
+            'password_confirmation' => 'password123'
+        ];
+
+        $response = $this->postJson('/api/auth/register', $payload);
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['name', 'email', 'password']);
+    }
 }
