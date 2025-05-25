62ca9cd98a25b519b48de4a8355c8947d015aca4
Eding Muhamad <masaruedo@yahoo.com>
2025-05-25 23:43:24 +0700

    SPA register user endpoint

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,4 +10,5 @@ Route::get('/', function () {
 
 Route::prefix('auth')->group(function() {
     Route::post('/login', LoginController::class);
+    Route::post('/register', RegisterController::class);
 });
