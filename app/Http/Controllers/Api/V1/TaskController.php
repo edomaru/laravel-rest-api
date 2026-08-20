e1828e594cf0bdd7f4edfd5cbc3cd8aa18459df7
Eding Muhamad <masaruedo@yahoo.com>
2026-08-20 17:50:22 +0700

    Update existing resource

--- app/Http/Controllers/Api/V1/TaskController.php
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -60,7 +60,9 @@ class TaskController extends Controller
      */
     public function update(UpdateTaskRequest $request, Task $task)
     {
-        //
+        $task->update($request->validated());
+
+        return $task->toResource();
     }
 
     /**
