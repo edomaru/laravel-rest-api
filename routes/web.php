c1d390537481885839996664adb004ab3adbe126
Eding Muhamad <masaruedo@yahoo.com>
2026-08-22 03:20:10 +0700

    Add SPA Logout endpoint

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
@@ -11,4 +12,5 @@ Route::get('/', function () {
 Route::prefix('auth')->group(function() {
     Route::post('/login', LoginController::class);
     Route::post('/register', RegisterController::class);
+    Route::post('/logout', LogoutController::class)->middleware('auth');
 });
