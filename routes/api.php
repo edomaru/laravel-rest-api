703f746a5d89a2e41fcc8c286d131b46adf768ee
Eding Muhamad <masaruedo@yahoo.com>
2026-08-21 08:18:51 +0700

    Create Login endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\LoginController;
 use App\Http\Controllers\Api\V1\CompleteTaskController;
 use App\Http\Controllers\Api\V1\TaskController;
 use Illuminate\Http\Request;
@@ -12,4 +13,8 @@ Route::get('/user', function (Request $request) {
 Route::prefix('v1')->group(function () {
     Route::apiResource('/tasks', TaskController::class);
     Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
+});
+
+Route::prefix('auth')->group(function () {
+    Route::post('/login', LoginController::class);
 });
