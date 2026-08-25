c0209cee773b699f9898b9a492306fc03656d37b
Eding Muhamad <masaruedo@yahoo.com>
2026-08-25 09:11:16 +0700

    Test the task summaries feature

--- app/Http/Controllers/Api/V2/SummaryController.php
+++ app/Http/Controllers/Api/V2/SummaryController.php
@@ -13,9 +13,9 @@ class SummaryController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $tasks = $request->user()->tasksSummary();
+        $tasks = $request->user()->tasksSummary($request->period);
  
-    return $tasks->mapToGroups(function ($item) {
+        return $tasks->mapToGroups(function ($item) {
             return [
                 $item->is_completed ? 'completed' : 'uncompleted' => TaskSummaryResource::make($item)
             ];
