fac39d7283385a70a59465259db347b3a29b1953
Eding Muhamad <masaruedo@yahoo.com>
2026-08-25 17:04:16 +0700

    Seed the tasks with due date

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Task;
 use App\Models\User;
+use Carbon\CarbonPeriod;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -16,8 +17,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $start = now()->startOfMonth()->subMonthsNoOverflow();
+        $end = now();
+        $period = CarbonPeriod::create($start, '1 day', $end);
+
         User::factory(5)
-            ->has(Task::factory()->count(10)->withRandomPriority())
-            ->create();
+            ->create()
+            ->each(function ($user) use($period) {
+                foreach ($period as $date) {
+                    $date->hour(rand(0, 23))->minute(rand(0, 6) * 10);
+
+                    Task::factory()
+                        ->withRandomPriority()
+                        ->withRandomDueDate()
+                        ->create([
+                            'user_id' => $user->id,
+                            'created_at' => $date,
+                            'updated_at' => $date
+                        ]);
+                }
+            });
     }
 }
