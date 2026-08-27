7b3bc3423fed0bc67cb518837a949bd457fd79c7
Eding Muhamad <masaruedo@yahoo.com>
2026-08-27 08:24:11 +0700

    Implmement PHP attribute for fillable

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,13 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+#[Fillable('name')]
 class Task extends Model
 {
     /** @use HasFactory<\Database\Factories\TaskFactory> */
     use HasFactory;
-
-    protected $fillable = ['name'];
 }
